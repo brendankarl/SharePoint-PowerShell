6538bfc4c19a1141069908b45d26d7ef81d5574d
Brendan Griffin <brendankgriffin@hotmail.com>
2016-01-27 09:32:56 +0000

    Added Get-UsersWithPermissionsToSite
    Provides a report of all users that have permission to a Site Collection

---
+++ Reporting/Get-UsersWithPermissionsToSite.ps1
@@ -0,0 +1,23 @@
+﻿$Site = Get-SPSite "http://intranet.contoso.com"
+$Output = "D:\SiteUsers.csv"
+"Display Name" + "," + "Username" + "," | Out-File -Encoding Default -FilePath $Output
+
+$UniquePerms = $Site.RootWeb.Users
+$AllUsers += $UniquePerms
+
+Foreach ($Web in $Site.AllWebs)
+{
+Write-Host "Processing:"$Web.URL -ForegroundColor Green
+    Foreach ($Group in ($Web.Groups))
+    {
+    Write-Host "-Checking:"$Group.Name
+    $Users = Get-SPUser -Web $Web.Url -Group $Group.Name
+    $AllUsers += $Users
+    }
+}
+
+$AllUsers = $AllUsers | Sort-Object | Get-Unique
+Foreach ($User in $AllUsers)
+{
+$User.DisplayName.Replace(",","") + "," + $User.UserLogin | Out-File -Encoding Default -FilePath $Output -Append
+}
